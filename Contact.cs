b3849b5d71a72810af6cc1a269dd937ff7ef32f2
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-03-07 22:51:06 -0500

    syntax clean up

--- Contact.cs
+++ Contact.cs
@@ -7,17 +7,15 @@ namespace addressBook
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        //public string FullName { get; set; }
+        public string FullName
+        {
+            get
+            { return $"{FirstName} {LastName}"; }
+        }
         public string Address { get; set; }
         public string Email { get; set; }
 
-        public Contact()
-        {
 
-            // FullName = $"{FirstName} {LastName}";
-
-
-        }
 
     }
 
